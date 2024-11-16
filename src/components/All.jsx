488a2f12d50c2a88db7ba2bf3d007bba1528b57a
Varshan <varshan5velan@gmail.com>
2024-11-16 11:53:39 +0530

    Push dashboard code to navbar branch

---
+++ src/components/All.jsx
@@ -0,0 +1,10 @@
+const All = () => {
+  return (
+    <div className="container">
+      <h1>All Courses</h1>
+      <p>Explore all the available courses to enhance your skills.</p>
+    </div>
+  );
+};
+
+export default All;
